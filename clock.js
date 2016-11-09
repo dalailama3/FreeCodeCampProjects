aa408ead9e121db073420c5bfaf12d6f50e48bf3
John Clark <johngclark36@gmail.com>
2016-11-09 16:00:01 -0500

    add handler on button click that calls a countdown function every second with setInterval, write #timeStrToSecs helper function

--- clock.js
+++ clock.js
@@ -13,10 +13,41 @@ $( document ).ready(function () {
     } else {
       curVal --;
     }
-    parent.html("<span>-</span>" + ' ' + curVal + ' ' + "<span>+</span>");
-    if (parent.attr("class") === 'timer-setter') {
-      $('.display').text(curVal + ':00');
+    if (curVal > 0 && curVal <= 90) {
+      parent.html("<span>-</span>" + ' ' + curVal + ' ' + "<span>+</span>");
+      if (parent.attr("class") === 'timer-setter') {
+        $('.display').text(curVal + ':00');
+      }
     }
+
+  });
+
+  var startButton = $("button");
+
+  var timeStrToSecs = function (str) {
+    if (str.length === 2) {
+      return parseInt(str) * 60;
+    } else {
+      var mins = parseInt(str.slice(0,2));
+      var secs = parseInt(str.slice(3));
+      return mins * 60 + secs;
+    }
+  }
+
+  var countDown = function (start) {
+    var seconds = timeStrToSecs(start)
+    seconds -= 1;
+    var mins = Math.floor(seconds/60);
+    var secs = seconds % 60;
+    $('.display').text(mins + ":" + secs);
+  };
+
+  startButton.on('click', function () {
+    setInterval(function () {
+      var startTime = $('.display').text();
+      var strTime = startTime.toString().trim();
+      countDown(strTime);
+    }, 1000);
   });
 
 });
